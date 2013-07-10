b5bd686ee994e0b4eb08f9ead465072c4bdc9855
Jason Campbell <jason@js.la>
2013-07-09 17:54:31 -0700

    Adds helper for cleaning a bucket

---
+++ test/support/clean.js
@@ -0,0 +1,53 @@
+
+var knox = require('knox')
+  , config = require('./config.json')
+  , concat = require('concat-stream')
+
+module.exports = clean
+
+function clean(bucket, callback){
+  var client = knox.createClient({ key: config.key
+      , secret: config.secret
+      , bucket: bucket
+      })
+
+  client.list(function(err, data){
+    if (err) return callback(err)
+
+    var zap = data['Contents']
+        .map(function(o){ return o['Key'] }) // only get the keys
+
+    if (zap.length === 0) return callback()
+
+    client.deleteMultiple(zap, function(err, res){
+      if (err) return callback(err)
+      if (res.statusCode < 400) return callback(null, res) // yey
+
+      // probably an error of some kind if things went this far
+      res.pipe(concat(function(data){
+        var ps = require('xml2js').parseString
+          , options = { explicitArray: false
+            , explicitRoot: false
+            }
+
+        ps(data, options, function(err, xml){
+          if (err) return callback(err)
+
+          console.log('xml', xml)
+          var message = 'AWS Responded '
+              + res.statusCode
+              + ': '
+              + xml['Code']
+              + ' => '
+              +  xml['Message']
+            , error = new Error(message)
+
+          res.body = xml
+
+          callback(error, res)
+        })
+      }))
+
+    })
+  })
+}
